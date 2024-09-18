f6eea6c94e1fe4ada637fdb579b8a1600a575678
Danishwer _ <dannywits12321@gmail.com>
2024-09-18 20:04:41 +0530

    First React Native Application

---
+++ app/_layout.js
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router";
+
+import React from "react";
+
+const _layout = () => {
+  return <Stack />;
+};
+
+export default _layout;
---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+import React from "react";
+
+function index(props) {
+  return (
+    <View>
+      <Text>First Application</Text>
+    </View>
+  );
+}
+
+export default index;
